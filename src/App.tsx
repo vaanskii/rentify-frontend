c86aa9ed2398caacfb3a7d75c7ff5ab21659d73c
vaanskii <giorgivanadze03@gmail.com>
2025-06-04 22:02:05 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navigation from './components/Navigation'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navigation />
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+const Navigation = () => {
+  return (
+    <> 
+      <div className="border-red-300 border w-20 hover:w-30">
+        <ul className="flex flex-col gap-2">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+ 
+export default Navigation;
